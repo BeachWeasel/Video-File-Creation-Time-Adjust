70f7bb0493367cda4302cec09f106c9bcc3f2e62
admin <admin@MOBILEE15>
2016-06-01 23:10:01 -0600

    First Commit

---
+++ VideoFileCreationTimeOriginalFromMetadata/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaInfoDotNet;
+using MediaInfoDotNet.Models;
+// try using parallel foreach for videos passed
+
+
+namespace VideoFileCreationTimeOriginalFromMetadata
+{
+    public class Video//FileInfo video)
+    {
+        public Video(FileInfo[] FI)
+        {
+           
+            Parallel.ForEach(FI, (f) =>
+             {
+                 try
+                 {
+                     DateTime dt = new DateTime();
+                     //look for encode date and time
+                     try
+                     {
+                         MediaInfoDotNet.MediaFile mf = new MediaFile(f.FullName); 
+
+                         dt = mf.General.encodedDate;
+                     }
+                     catch
+                     {
+                         //if encode date and time doesn't exist 
+                     }
+
+
+                     //if encode date and time exists compare to video's fileinfo creation date and time
+                     if (dt != f.CreationTime)
+                     {
+                         // if different update video's fileinfo with video's encode date and time 
+                        
+                         DateTime ldt = new DateTime(dt.Year,dt.Month,dt.Day,dt.Hour,dt.Minute,dt.Second);
+                         f.CreationTime =ldt;
+                     }
+
+
+                 }
+                 catch
+                 { }
+             });
+
+        }
+
+        
+
+    }
+}
---
+++ tesingform/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoFileCreationTimeOriginalFromMetadata;
+
+namespace tesingform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {               
+                FileInfo[] a = ofd.FileNames.Select(f => new FileInfo(f)).ToArray() ; // found stack overflow thread advising against using fileinfo array. Not sure why...
+                Video v = new Video(a);
+            }
+        }
+    }
+}
